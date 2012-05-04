60a8a4a090e88f10817a4e8f2e2d6812184cd63a
DAI Zhiwei <shinysky1986_AT_gmail.com>
2012-05-04 11:18:04 +0800

    adjusted directories

---
+++ icm-ios-agent/ICMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICMAppDelegate.h
+//  icm-ios-agent
+//
+//  Created by shinysky on 12-5-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
